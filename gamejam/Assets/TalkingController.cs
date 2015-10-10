d61e5905299ea235dd10cdf4ef3021b07330c281
Stuart G <mr.stubob@gmail.com>
2015-10-10 13:06:10 -0600

    no message

---
+++ gamejam/Assets/TalkingController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TalkingController : MonoBehaviour
+{
+
+    public GameObject Charater1;
+    public GameObject Charater2;
+
+    private Animator char1Anim;
+    private Animator char2Anim;
+    // Use this for initialization
+    void Start()
+    {
+        char1Anim = Charater1.GetComponent<Animator>();
+        char2Anim = Charater2.GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        char1Anim.enabled = false;
+    }
+}
