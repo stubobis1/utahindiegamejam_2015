ef8ca065cfd66c9481086f488f2e0f322ab8c6ec
Steven Tran <St.Tran6@gmail.com>
2015-10-10 11:37:35 -0600

    Animations back in, working on switching areas

--- gamejam/Assets/PlayerControl.cs
+++ gamejam/Assets/PlayerControl.cs
@@ -28,7 +28,6 @@ public class PlayerControl : MonoBehaviour
     {
         // Setting up references.
 
-
         groundCheck = transform.Find("groundCheck");
         groundedScript = groundCheck.GetComponent<grounded>();
         anim = GetComponent<Animator>();
@@ -46,7 +45,6 @@ public class PlayerControl : MonoBehaviour
         {
             jump = true;
         }
-
 
         if (Input.GetButtonDown("Fire2"))
         {
@@ -69,7 +67,7 @@ public class PlayerControl : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
 
         // The Speed animator parameter is set to the absolute value of the horizontal input.
-        //anim.SetFloat("Speed", Mathf.Abs(h));
+        anim.SetFloat("Speed", Mathf.Abs(h));
 
         // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
         if (h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
---
+++ gamejam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject zone1;
+    public GameObject zone2;
+
+	void Awake () {
+        if (zone1 == null || zone2 == null)
+            Debug.LogError("ZONES MISSING IN GAME MANAGER");
+
+        SpriteRenderer[] spriteRenderers = zone1.GetComponentsInChildren<SpriteRenderer>();
+
+        Debug.Log(spriteRenderers.Length);
+
+        foreach (SpriteRenderer s in spriteRenderers)
+            s.sortingLayerName = "Front";
+
+        spriteRenderers = zone2.GetComponentsInChildren<SpriteRenderer>();
+
+        foreach (SpriteRenderer s in spriteRenderers)
+            s.sortingLayerName = "Back";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
