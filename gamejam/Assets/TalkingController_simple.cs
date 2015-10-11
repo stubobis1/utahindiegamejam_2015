f72d6fb075a6d300098ba72791cb4c49a3479b1e
Stuart G <mr.stubob@gmail.com>
2015-10-11 00:40:12 -0600

    no message

---
+++ gamejam/Assets/TalkingController_simple.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TalkingController_simple : MonoBehaviour
+{ 
+    public AudioSource source;
+    public AudioClip[] audios;
+
+    // Use this for initialization
+    void Start()
+    {
+     
+    }
+
+
+    // Update is called once per frame
+    int currentAudio = 0;
+    void Update()
+    {
+        if (!source.isPlaying)
+        {
+            if (currentAudio < audios.Length)
+            {
+                source.PlayOneShot(audios[currentAudio]);
+                currentAudio++;
+            }
+            else
+            {
+                Application.LoadLevel(Application.loadedLevel +1);
+            }
+        }
+    }
+}
