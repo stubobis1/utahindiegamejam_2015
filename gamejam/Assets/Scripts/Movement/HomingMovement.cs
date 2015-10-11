cdb2f326d643845b05e4b72c4d9edac7a6f21d2f
Steven Tran <St.Tran6@gmail.com>
2015-10-11 11:20:58 -0600

    Raycast Ground Detection & other stuff
    -Improved jumping detection, but jump force is still inconsistent
    -No more jumping on air
    -Fixed other stuff

--- gamejam/Assets/Scripts/Movement/HomingMovement.cs
+++ gamejam/Assets/Scripts/Movement/HomingMovement.cs
@@ -10,12 +10,11 @@ public class HomingMovement : EnemyMovement {
     public float maxHomingDistance = 1000.0f;
     bool targetAcquired = false;
     float retargetTimer = 0.0f;
+    public float retargetDelay = 0.5f;
     Vector2 cachedVelocity;
 
 	public override void Start () {
         base.Start();
-
-        speed = 2.0f;
 
         if (peekaboo)
             disabled = true;
@@ -29,7 +28,6 @@ public class HomingMovement : EnemyMovement {
             if (target)
                 targetAcquired = true;
         }
-            
 
         if (peekaboo && target)
         {
@@ -49,7 +47,7 @@ public class HomingMovement : EnemyMovement {
         {
             if (target && retargetTimer <= 0.0f)
             {
-                retargetTimer += 1.0f;
+                retargetTimer = retargetDelay;
                 Vector3 targetVector = (target.transform.position - gameObject.transform.position).normalized;
                 direction = new Vector2(targetVector.x, targetVector.y);
             }
@@ -62,6 +60,23 @@ public class HomingMovement : EnemyMovement {
             GetComponent<Rigidbody2D>().AddForce(direction * speed);
         }
 	}
+
+    public override void FixedUpdate()
+    {
+        if(target && !disabled)
+        {
+            float boundsLength = Mathf.Max(GetComponent<Collider2D>().bounds.extents.x,
+                GetComponent<Collider2D>().bounds.extents.y);
+            float targetBoundsLength = Mathf.Max(target.GetComponent<Collider2D>().bounds.extents.x,
+                target.GetComponent<Collider2D>().bounds.extents.y);
+
+            if ((transform.position - target.transform.position).magnitude <=
+                boundsLength + targetBoundsLength)
+                target.BroadcastMessage("Death");
+        }
+
+        base.FixedUpdate();
+    }
 
     public override void Enable()
     {
--- gamejam/Assets/grounded.cs
+++ gamejam/Assets/grounded.cs
@@ -5,21 +5,30 @@ public class grounded : MonoBehaviour {
 
     [HideInInspector]
     public bool isGrounded = false;
-	// Use this for initialization
+
+    [HideInInspector]
+    public bool isGrounded2 = false;
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void FixedUpdate() {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(gameObject.transform.position, new Vector2(0, -1),
+            gameObject.GetComponent<Collider2D>().bounds.extents.y + 0.8f, 1 << gameObject.layer);
 
-	}
-    void OnTriggerEnter2D(Collider2D coll)
-    {
-        if (coll.gameObject.tag == "Ground")
+        if (hits.Length <= 0)
+            isGrounded = false;
+
+        foreach(RaycastHit2D rh in hits)
         {
-            isGrounded = true;
+            if (rh.transform.tag == "Ground")
+            {
+                isGrounded = true;
+                break;
+            }
+            else
+                isGrounded = false;
         }
-    }
-    
+	}
 }
