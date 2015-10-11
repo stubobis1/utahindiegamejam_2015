df0703e18d086fa5cb1caf40b5e4ca61d83d87e6
Stuart G <mr.stubob@gmail.com>
2015-10-10 22:23:18 -0600

    no message

---
+++ gamejam/Assets/KillPlane.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
